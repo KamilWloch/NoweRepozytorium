045d02849840477b2a72f0f719f54f305e592f90
KamilWloch <109965679+KamilWloch@users.noreply.github.com>
2023-07-11 20:41:56 +0200

    Zad 1
    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector("ul.gallery");
+
+const galleryImages = galleryItems
+    .map(image => 
+        `<li class = "gallery__item">
+            <a class = "gallery__link" href = "${image.original}">
+                <img class = "gallery__image" src = "${image.preview}" alt = "${image.description}" data-alt = "${ image.description } &#8212 ${ image.original }" />
+            </a>
+        </li>`)
+    .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryImages);
+console.log(gallery);
+
+new SimpleLightbox(".gallery a", {
+    animationSpeed: 500,
+    captionsData: "data-alt",
+    captionDelay: 250,
+    download: "If you want download this picture please click here...",
+    doubleTapZoom: 4,
+    maxZoom: 20,
+    scrollZoomFactor: 0.1,
+});
