c3419f4530486a9dc841d1094fffcff5926974fe
KamilWloch <109965679+KamilWloch@users.noreply.github.com>
2023-07-11 22:11:42 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,18 @@
-import Player from '@vimeo/player';
-import { throttle } from 'lodash';
+import Vimeo from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
 
-player.on('timeupdate', throttle(({ duration, percent, seconds }) => {
-    localStorage.setItem('videoplayer-current-time', `${seconds}`);
-}, 1000));
+const player = new Vimeo('vimeo-player');
 
-const actualTime = localStorage.getItem('videoplayer-current-time');
+player.on('loaded', () => {
+  const currentTime = localStorage.getItem(LOCAL_STORAGE_KEY) || 0;
+  player.setCurrentTime(currentTime);
+});
 
-player
-    .setCurrentTime(actualTime)
-    .then(function (seconds) { })
-    .catch(function (error) {
-        switch (error.name) {
-            case 'RangeError':
-                break;
-            default:
-                break;
-        }
-    });
+player.on(
+  'timeupdate',
+  _throttle(({ seconds }) => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, seconds);
+  }, 1000)
+);
